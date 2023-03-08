f40cd125eaac861db9325f55d699635a5eebd8fa
Godwin Segun <godwinefesegun@gmail.com>
2023-03-08 13:33:34 -0800

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -12,6 +12,7 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0')
 	{
+
 		if (*s2 != '\0' && *s2 == '*')
 		{
 			return (wildcmp(s1, s2 + 1));
@@ -22,7 +23,8 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
-	else if (*s1 == *s2){
+	else if (*s1 == *s2)
+	{
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	return (0);
