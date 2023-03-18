c2d6da38832d83900fa768ba01e853666a1f7921
Godwin Segun <godwinefesegun@gmail.com>
2023-03-18 13:22:00 -0700

    0x09-static_libraries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _puts - prints a string, followed by a new line.
+ *@s: input string.
+ * Return: no return.
+ */
+
+void _puts(char *s)
+{
+	puts(s);
+}
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: char to be printed
+ * Return: 1 on success
+ */
+
+int _putchar(char c)
+{
+	return (putchar(c));
+}
