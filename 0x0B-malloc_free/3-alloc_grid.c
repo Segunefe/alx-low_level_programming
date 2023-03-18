dea51aba2749d9e201a4621765d9e27536f9c8b0
Godwin Segun <godwinefesegun@gmail.com>
2023-03-18 16:36:49 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,23 +17,24 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 
 	array = (int **)malloc(sizeof(int *) * height);
-	if (array == NULL)
-		return (NULL);
+		if (array == NULL)
+			return (NULL);
 
-	for (i = 0; i < height; i++)
-	{
-		array[i] = (int *)malloc(sizeof(int) * width);
-		if (array[i] == NULL)
+		for (i = 0; i < height; i++)
+		{
+			array[i] = (int *)malloc(sizeof(int) * width);
+			if (array[i] == NULL)
+			{
+				for (i--; i >= 0; i--)
+					free(array[i]);
+				free(array);
+				return (NULL);
+			}
+		}
 
-			for (i--; i >= 0; i--)
-				free(array[i]);
-		free(array);
-		return (NULL);
-	}
-}
+		for (i = 0; i < height; i++)
+			for (j = 0; j < width; j++)
+				array[i][j] = 0;
 
-for (i = 0; i < height; i++)
-for (j = 0; j < width; j++)
-array[i][j] = 0;
-return (array);
+		return (array);
 }
